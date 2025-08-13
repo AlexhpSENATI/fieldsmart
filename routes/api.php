72b83811d00cf71838070ace85bf27e1d5dadeba
AlexhpSENATI <1510696@senati.pe>
2025-08-12 22:17:26 -0500

    error en api.php

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use Illuminate\Support\Facades\Log;
+
+
+Route::post('/sensores', function (Request $request) {
+Log::info('Datos recibidos:', $request->all());
+  // Para ver los datos en el log
+    return response()->json(['status' => 'ok', 'data' => $request->all()]);
+});
