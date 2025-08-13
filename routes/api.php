a8d439784dd255a5d348c4e81749d4ecfe8f2860
AlexhpSENATI <1510696@senati.pe>
2025-08-12 23:13:43 -0500

    resiviendo datos de sensor mediante ip

--- routes/api.php
+++ routes/api.php
@@ -9,4 +9,5 @@ Route::post('/sensores', function (Request $request) {
 Log::info('Datos recibidos:', $request->all());
   // Para ver los datos en el log
     return response()->json(['status' => 'ok', 'data' => $request->all()]);
+    
 });
